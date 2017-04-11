5fcb07afb8806f60d3c8584efb1b5402bde6d1f0
gezi <1434216023@qq.com>
2017-04-11 23:53:12 +0800

    hello py

---
+++ query_ticket.py
@@ -0,0 +1,16 @@
+from stations import stations
+
+def cli():
+    arguments = docopt(__doc__)
+    from_station = stations.get(arguments['<from>'])
+    to_station = stations.get(arguments['<to>'])
+    date = arguments['<date>']
+    # 构建URL
+    url = 'https://kyfw.12306.cn/otn/leftTicket/query?leftTicketDTO.train_date={}&leftTicketDTO.from_station={}&leftTicketDTO.to_station={}&purpose_codes=ADULT'.format(
+        date, from_station, to_station
+    )
+
+def cli():
+    ...
+    r = requsets.get(url);
+    print(r.json())
